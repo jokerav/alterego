2eeea0acfb3604a4c105691ea5ddbb9602cff852
jokerav <jokeravdeveloper@gmail.com>
2023-02-18 18:15:55 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ function App() {
   return (
       <Routes>
         <Route path='/' element={<p>Main page</p>}/>
-        <Route path='/news' element={<p>News</p>}/>
+        <Route path='/news' element={<p>NEWS</p>}/>
+        <Route path='/profile' element={<p>PROFILE</p>}/>
       </Routes>
   );
 }
