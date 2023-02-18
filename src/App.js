9c2d483c4ffa7d81430930f732c990c9e67ba8d6
jokerav <jokeravdeveloper@gmail.com>
2023-02-18 21:57:25 +0200

    AppBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
+import ResponsiveAppBar from "./components/AppBar/AppBar";
 
 function App() {
   return (
       <Routes>
-        <Route path='/' element={<p>Main page</p>}/>
+        <Route path='/' element={<ResponsiveAppBar/>}/>
         <Route path='/news' element={<p>NEWS</p>}/>
         <Route path='/profile' element={<p>PROFILE</p>}/>
       </Routes>
