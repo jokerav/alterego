30369cc170552205ef45258a97205e151bf5ada5
jokerav <jokeravdeveloper@gmail.com>
2023-02-20 22:32:36 +0200

    Update NewsPage.js

--- src/components/NewsPage.js
+++ src/components/NewsPage.js
@@ -1,11 +1,16 @@
 import {useGetNewsQuery} from "../redux/fakeNewsAPI";
 import OutlinedCard from "./OutlinedCard";
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+
 
 const NewsPage = () => {
     const {data: news = [], isError, isLoading} = useGetNewsQuery();
     return (
         <div>
-            {isLoading && <p>Завантажуємо...</p>}
+            {isLoading && <Box sx={{ display: 'flex' }} >
+                <CircularProgress size={200} sx={{position: "absolute", left: "50%"}}/>
+            </Box>}
             {isError && <p>Помилочка...</p>}
             {news.length > 0 && news.map(
                 (newsItem) => (<OutlinedCard key={newsItem.id} newsItem={newsItem}/>)
