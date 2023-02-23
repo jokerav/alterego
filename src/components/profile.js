c9d95564d5cf53784eda3ce06ed6bc3b6212229f
jokerav <jokeravdeveloper@gmail.com>
2023-02-23 13:22:33 +0200

    Update profile.js

--- src/components/profile.js
+++ src/components/profile.js
@@ -49,8 +49,6 @@ const ProfilePage = () => {
                                     />
                                 </ListItem>
                             )}
-
-
                         </List>
                     </Demo>:
                         <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
